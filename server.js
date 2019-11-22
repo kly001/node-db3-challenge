076a70947fd8ba95a8f17279add73ce3f0d05264
kly001 <karenly1961@gmail.com>
2019-11-21 19:16:19 -0500

    queries started

--- server.js
+++ server.js
@@ -7,4 +7,8 @@ const server = express();
 server.use(express.json());
 server.use('/api/schemes', SchemeRouter);
 
+server.get('/',(req,res) => {
+    res.send("<h1>node-db3-challenge</h1>")
+  })
+
 module.exports = server;
